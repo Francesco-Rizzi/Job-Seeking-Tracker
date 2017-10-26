a81d7423b4fb127cdcb92868c9c3c5bbc8ee3472
Francesco Rizzi <rizzifrancesco@icloud.com>
2017-10-26 22:05:06 +0200

    RELATED BRANCH: master; MESSAGE: backend scaffolding

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 	require_once __DIR__ . '/secrets.php';
 	require_once __DIR__ . '/utils.php';
 	
+	use \Symfony\Component\HttpFoundation\Request;
+	
 	$app = new Silex\Application();
+	connectToDB($app);
 	
 	$app[ 'debug' ] = IS_DEBUG;
 	
@@ -16,11 +19,48 @@
 		
 		return file_get_contents('./frontend/dist/index.html');
 		
-	})->assert('route', '^(|app|features)$');
+	})
+		->assert('route', '^(|app|features)$');
 	
 	/*
 	 * API SECTION
 	 * */
+	
+	$app->get('/get-user-data', function ( Request $request ) use ( $app ){
+		
+		$JWT  = $request->request->get('JWT');
+		$data = [ 'JWT' => fetchUserData($JWT) ];
+		
+		return $app->json($data, 200);
+		
+	});
+	
+	$app->post('/save-user-data', function ( Request $request ) use ( $app ){
+		
+		$data = [];
+		
+		return $app->json($data, 200);
+		
+	});
+	
+	$app->post('/signup', function ( Request $request ) use ( $app ){
+		
+		$name     = $request->request->get('name');
+		$email    = $request->request->get('email');
+		$password = $request->request->get('password');
+		$data     = createNewUser($name, $email, $password, $app);
+		
+		return $app->json($data, 200);
+		
+	});
+	
+	$app->post('/signin', function ( Request $request ) use ( $app ){
+		
+		$data = [];
+		
+		return $app->json($data, 200);
+		
+	});
 	
 	$app->run();
 	
--- utils.php
+++ utils.php
@@ -1,29 +1,111 @@
 <?php
 	
-	function createJWT(){
+	use Lcobucci\JWT\Builder as JWTBuilder;
+	use Lcobucci\JWT\Parser as JWTParser;
+	use Lcobucci\JWT\ValidationData as JWTValidator;
+	use Silex\Provider\DoctrineServiceProvider as DoctrineProvider;
+	
+	function createJWT( $userID ){
+	
 	
 	}
 	
-	function logIn(){
-	
+	function logIn( $email, $password, $app ){
+		
+		if ( !$email ) {
+			return [ 'error' => 'Please provide your email.' ];
+		}
+		if ( !$password ) {
+			return [ 'error' => 'Please provide your password.' ];
+		}
+		
+		$user = $app[ 'db' ]->fetchAssoc('SELECT * FROM users WHERE email = ?', [ $email ]);
+		
+		if ( $user === FALSE ) {
+			return [ 'error' => 'User not found.' ];
+		}
+		
+		if ( password_verify($password, $user[ 'password' ]) ) {
+			
+			return createJWT($user[ 'id' ]);
+			
+		}
+		
+		return [ 'error' => 'Login credentials are not correct.' ];
+		
 	}
 	
-	function createNewUser(){
-	
+	function createNewUser( $name, $email, $password, $app ){
+		
+		if ( !$name ) {
+			return [ 'error' => 'Please provide your name.' ];
+		}
+		if ( !$email ) {
+			return [ 'error' => 'Please provide your email.' ];
+		}
+		if ( !$password ) {
+			return [ 'error' => 'Please provide your password.' ];
+		}
+		
+		$rows = $app[ 'db' ]->fetchAll('SELECT * FROM users WHERE email = ?', [ $email ]);
+		if ( $rows !== FALSE ) {
+			return [ 'error' => 'Email already in use.' ];
+		}
+		
+		$password = encryptPassword($password);
+		
+		$app[ 'db' ]->insert('users', [ 'username' => $name,
+										'email'    => $email,
+										'password' => $password,
+										'data'     => "" ]);
+		$ID = $app[ 'db' ]->lastInsertId();
+		
+		return [ 'JWT' => createJWT($ID) ];
+		
 	}
 	
-	function saveUserData(){
-	
+	function saveUserData( $JWT, $data, $app ){
+		
+		$ID = getUserIDFromJWT($JWT);
+		
+		$rowsAffected = $app[ 'db' ]->update('users', [ 'data' => $data ], [ 'id' => $ID ]);
+		
+		if ( $rowsAffected !== 1 ) {
+			return [ 'error' => 'Something went wrong, please retry or contact the author.' ];
+		}
+		
+		return [];
+		
 	}
 	
-	function fetchUserData(){
-	
+	function fetchUserData( $JWT, $app ){
+		
+		$ID   = getUserIDFromJWT($JWT);
+		$user = $app[ 'db' ]->fetchAssoc('SELECT * FROM users WHERE id = ?', [ $ID ]);
+		
+		return [ 'data' => $user[ 'data' ] ];
+		
 	}
 	
-	function getUserFromJWT( $JWT ){
-	
+	function getUserIDFromJWT( $JWT ){
+		
+		return 1;
+		
 	}
 	
-	function connectToDB(){
+	function encryptPassword( $password ){
+		
+		return password_hash($password, PASSWORD_BCRYPT);
+		
+	}
 	
+	function connectToDB( $app ){
+		
+		$app->register(new DoctrineProvider(), [ 'db.options' => [ 'dbname'   => DB_NAME,
+																   'user'     => DB_USER,
+																   'password' => DB_PASSWORD,
+																   'host'     => 'localhost',
+																   'driver'   => 'pdo_mysql',
+																   'port'     => 3306 ] ]);
+		
 	}
